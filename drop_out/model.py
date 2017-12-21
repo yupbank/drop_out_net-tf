7aa691c0cff8155f63915fe5421507bfe84a4e0e
Peng Yu <peng.yu@shopify.com>
2017-12-21 16:06:39 -0500

    add model

---
+++ drop_out/model.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+
+slim = tf.contrib.slim
+
+def deep_cf_model(u, v, u_content, v_content, rank_out=200, model_layers=[800, 400], is_training=True):
+    """
+    u: user embedded by wmf model 
+    v: item embedded by wmf model 
+    user_content: user content info
+    item_content: item content info
+    rank_out: dimension of out
+    """
+
+    u_concat = tf.concat([u, u_content], 1)
+    v_concat = tf.concat([v, v_content], 1)
+
+    with slim.arg_scope([slim.fully_connected], 
+                        activation_fn=tf.nn.tanh,
+                        normalizer_fn=slim.batch_norm,
+                        normalizer_params=dict(decay=0.9,
+                                                center=True,
+                                                scale=True,
+                                                is_training=is_training),
+                        ):
+        u_last = slim.stack(u_concat, slim.fully_connected, model_layers, scope='user_layer')
+        v_last = slim.stack(v_concat, slim.fully_connected, model_layers, scope='item_layer')
+
+    with slim.arg_scope([slim.fully_connected], 
+                        activation_fn=None):
+
+        u_emb = slim.fully_connected(u_last, rank_out)
+        v_emb = slim.fully_connected(v_last, rank_out)
+            
+    pred = tf.multiply(u_emb, v_emb)
+
+    return pred
+
+
+def wmf(pairs, weight):
+    return
